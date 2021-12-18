4fda97ecba69ba245547715c574fb5daacdb04f1
lucaslabre <lucaslabre@gmail.com>
2021-12-18 13:05:42 -0300

    Criação do Auth Guard

--- Japastel-Angular/src/app/app-routing.module.ts
+++ Japastel-Angular/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './feature/login/pages/login-page/login-page.component';
 import { PedidosPageComponent } from './feature/pedidos/pages/pedidos-page/pedidos-page.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'pedidos', component: PedidosPageComponent }
+  { path: 'pedidos', component: PedidosPageComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
