42c5fde4d642d9928e5f96b6a5f8168c4831e425
lucaslabre <lucaslabre@gmail.com>
2021-12-19 11:01:01 -0300

    Inclusão ReactiveForm no login

--- Japastel-Angular/src/app/feature/login/pages/login-page/login-page.component.ts
+++ Japastel-Angular/src/app/feature/login/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -7,9 +8,19 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    cpf: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit () {
+    sessionStorage.setItem('currentUser', JSON.stringify(this.loginForm.value));
+    this.router.navigateByUrl('/pedidos');
   }
 
 }
