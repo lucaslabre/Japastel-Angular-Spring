3891765bec108255cee11e5eb5223f88849102a4
lucaslabre <lucaslabre@gmail.com>
2021-12-20 20:10:50 -0300

    Criada funcionalidade Sair

--- Japastel-Angular/src/app/shared/components/header/header.component.ts
+++ Japastel-Angular/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/feature/user/models/user.model';
 import { UserService } from 'src/app/feature/user/services/user.service';
 
@@ -12,6 +13,7 @@ export class HeaderComponent implements OnInit {
   user?: User;
 
   constructor(
+    private router: Router,
     private userService: UserService
   ) { }
 
@@ -20,6 +22,11 @@ export class HeaderComponent implements OnInit {
     if (userStorage) {
       this.user = this.userService.getUserById(JSON.parse(userStorage));
     }
+  }
+
+  exit() {
+    sessionStorage.clear();
+    this.router.navigateByUrl('/login');
   }
 
 }
